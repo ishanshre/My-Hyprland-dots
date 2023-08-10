e09200bf544cc717d969a427cc175f5ce1a89d27
Ishan Shrestha <75682546+ishanshre@users.noreply.github.com>
2023-08-10 18:54:37 +0545

    fix: fix chaotic repo

--- chaotic-repo.sh
+++ chaotic-repo.sh
@@ -2,8 +2,8 @@
 
 echo "+++++Adding chaotic repo+++++"
 
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo "\
 [chaotic-aur]

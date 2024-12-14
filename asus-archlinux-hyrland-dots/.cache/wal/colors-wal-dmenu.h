5a5a51d497730ad81a71ae6bb6cd2b07a9955896
Ishan Shrestha <ishan.shrestha356@gmail.com>
2024-12-14 18:52:23 +0545

    fix: store config only, remove others

--- asus-archlinux-hyrland-dots/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#8fd4d3", "#05870A" },
-	[SchemeSel] = { "#8fd4d3", "#5D5752" },
-	[SchemeOut] = { "#8fd4d3", "#103D99" },
-};
--- asus-archlinux-hyrland-dots/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#05870A";
-static const char* selfgcolor   = "#8fd4d3";
-static const char* normbgcolor  = "#8AD072";
-static const char* normfgcolor  = "#8fd4d3";
-static const char* urgbgcolor   = "#5D5752";
-static const char* urgfgcolor   = "#8fd4d3";

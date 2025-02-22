7b3db84147f2936ab2377e734d84b8cb694fcb0d
Ishan Shrestha <ishan.shrestha356@gmail.com>
2025-02-22 19:39:40 +0545

    fix: update nvim dots

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -57,6 +57,7 @@ require("lazy").setup({
     { import = "plugins.gitsigns" },
     { import = "plugins.markdown_render" },
     { import = "plugins.cursorline" },
+    -- { import = "plugins.evangelion" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
